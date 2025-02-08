8e31bd4f9f328061ebcab43e7f9b3f4fdb715c02
Abdurahmon Mirmaxsudov <abdurahmonmirmaxsudov2804@gmail.com>
2025-02-08 10:28:47 +0500

    commit

--- src/layout/HomeLayout.jsx
+++ src/layout/HomeLayout.jsx
@@ -3,6 +3,7 @@ import Nav from "./../components/nav/Nav";
 import Sort from "./../components/sort/Sort";
 import { Outlet } from "react-router-dom";
 import Brand from "../components/brand/Brand";
+import Footer from './../components/footer/Footer';
 
 const HomeLayout = () => {
   return (
@@ -10,6 +11,7 @@ const HomeLayout = () => {
       <Nav />
       <Sort />
       <Outlet />
+      <Footer />
     </>
   );
 };
